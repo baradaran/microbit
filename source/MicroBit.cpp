c8ade041d7ca9051b8791685a88ab0f24e25d079
Shubham Agarwal <47361474+shubh401@users.noreply.github.com>
2019-10-17 12:39:54 +0200

    Update MicroBit.cpp

--- source/MicroBit.cpp
+++ source/MicroBit.cpp
@@ -70,7 +70,6 @@ MicroBit::MicroBit() :
     buttonAB(MICROBIT_ID_BUTTON_A,MICROBIT_ID_BUTTON_B, MICROBIT_ID_BUTTON_AB),
     accelerometer(i2c),
     compass(i2c,accelerometer,storage),
-    compassCalibrator(compass, accelerometer, display),
     thermometer(storage),
     io(MICROBIT_ID_IO_P0,MICROBIT_ID_IO_P1,MICROBIT_ID_IO_P2,
        MICROBIT_ID_IO_P3,MICROBIT_ID_IO_P4,MICROBIT_ID_IO_P5,
